bda561b3fe5e51606f13e014926d0024278e76d9
ejwaple <122317503+ejwaple@users.noreply.github.com>
2023-02-14 17:11:34 -0500

    format

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.getElementById('add').addEventListener('click', function(){
+	var xdropdown = document.getElementById("xdropdown");
+	var newx = xdropdown.value;
 
+	var ydropdown = document.getElementById("ydropdown");
+	var newx = ydropdown.value;
+}
