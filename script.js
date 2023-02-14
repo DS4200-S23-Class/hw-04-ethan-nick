d29047f83b2ae291f9d754191874ab564c16e398
ejwaple <122317503+ejwaple@users.noreply.github.com>
2023-02-14 18:56:26 -0500

    js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function updatescore() {
+    document.getElementById("recent").innerHTML = "UPDATED";
+}
+
+function highlight() {
+	document.getElementById("point").style.stroke="orange";
+}
+
+/**
 document.getElementById('add').addEventListener('click', function(){
 	var xdropdown = document.getElementById("xdropdown");
 	var newx = xdropdown.value;
@@ -5,7 +14,4 @@ document.getElementById('add').addEventListener('click', function(){
 	var ydropdown = document.getElementById("ydropdown");
 	var newx = ydropdown.value;
 }
-
-function updatescore() {
-      document.getElementById("recent").innerHTML = "UPDATED";
-}
+**/ 
