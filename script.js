564d176f5e4bfbb6d839b2a0a0c79ebe431a93be
ejwaple <122317503+ejwaple@users.noreply.github.com>
2023-02-14 19:37:30 -0500

    plot function

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-function updatescore() {
+function plot() {
     document.getElementById("recent").innerHTML = "UPDATED";
+    var xdropdown = document.getElementById("xdropdown");
+	var newx = xdropdown.value;
+
+	var ydropdown = document.getElementById("ydropdown");
+	var newy = ydropdown.value;
+
+	alert(newy)
+
+	
 }
 
-function highlight(point_id)
-  {
+function highlight(point_id) {
       document.getElementById(point_id).style.stroke="orange";
       document.getElementById("recent").innerHTML = point_id;
   }
 
-/**
-document.getElementById('add').addEventListener('click', function(){
-	var xdropdown = document.getElementById("xdropdown");
-	var newx = xdropdown.value;
 
-	var ydropdown = document.getElementById("ydropdown");
-	var newx = ydropdown.value;
-}
-**/ 
+
+
